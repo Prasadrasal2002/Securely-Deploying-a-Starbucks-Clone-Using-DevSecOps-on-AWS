2f1b49104bc00ce4e3fdc1d6b1336815bfecaa19
Prasadrasal2002 <145026242+Prasadrasal2002@users.noreply.github.com>
2025-04-17 01:42:54 +0530

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -1,30 +1,45 @@
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.0.0"
+provider "aws" {
+  region = "ap-south-1"
+}
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
-  vpc_id          = var.vpc_id
+# EKS Cluster Resource
+resource "aws_eks_cluster" "eks" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids             = var.subnet_ids
+    endpoint_public_access = true
+    endpoint_private_access = false
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy]
+}
+
+# EKS Node Group Resource
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "vaishnavi-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.subnet_ids
 
-  cluster_endpoint_public_access = true
-
-  cluster_iam_role_name = var.eks_role_name
-  cluster_iam_role_arn  = aws_iam_role.eks_cluster_role.arn
-
-  node_groups = {
-    default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_types = ["t3.medium"]
-      iam_role_arn   = aws_iam_role.eks_node_role.arn
-    }
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+  instance_types = ["t3.medium"]
+
+  depends_on = [aws_eks_cluster.eks]
+}
+
+# Security Group Rule to Allow Jenkins to Access EKS
+resource "aws_security_group_rule" "allow_jenkins_to_eks" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
+  source_security_group_id = "sg-0e25d3fcef16c2162"  # Replace this
 }
